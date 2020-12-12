8eebbab001c29aef80e8b358ef47c397048c63eb
Louis.Su <louis.su@appimc.com>
2020-12-13 01:26:15 +0800

    Add database migration

---
+++ database/migrations/2020_12_12_093452_create_areas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAreasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('areas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('city_id')->constrained('cities');
+            $table->string('zip');
+            $table->string('filename');
+            $table->string('area');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('areas');
+    }
+}
